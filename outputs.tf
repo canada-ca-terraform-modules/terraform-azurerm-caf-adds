a3868d9aecb328e6d6c18f1686e10ede33fd1075
John Nephin <john.nephin@ssc-spc.gc.ca>
2020-07-29 17:36:51 -0400

    fixed source and outputs when deploy is false

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "dc1" {
-  value = module.dc1
+  value = var.deploy ? module.dc1 : null
 }
 
 output "dc2" {
-  value = module.dc2
+  value = var.deploy ? module.dc2 : null
 }
 
 output "domain" {
-  value = var.ad_domain_name
+  value = var.deploy ? var.ad_domain_name : null
 }
